ca11d5148230e9fa308b0e5c61fb9d09e43b703c
AmanBalyan20 <19ca1001@mvn.edu.in>
2023-09-15 22:38:09 +0530

    V 1.0.1

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
-const app = express;
+const app = express();
 
 //Now load all the configuration from the env file
-const port = process.env.port || 4000;
+require("dotenv").config();
+const port = process.env.port || 3011;
 
-app.request(express.json());
+
+require("dotenv").config();
+app.use(express.json());
 
 const todoRoute =require('./routes/TodoRoute');
 
-app.request('./v1',todoRoute);
+app.use('/v1',todoRoute);
 
-app.listen(port , ()=> {
+app.listen(port, ()=> {
     console.log("Your Server has been started ")
 })
 
 const dbConnect = require("./config/dbConnection");
 dbConnect();
 
-app.length("/",(req,res)=>{
+app.get("/",(req,res)=>{
     res.send('Welcome to the home');
 })
