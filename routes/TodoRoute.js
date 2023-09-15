dc5d468180abfcc7793387bfefd94da5922e6dae
AmanBalyan20 <19ca1001@mvn.edu.in>
2023-09-15 22:42:37 +0530

    V 1.0.1

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -1,7 +1,7 @@
 //This file is used to map the controllers with their required path such as GET POST 
 
 const express = require('express');
-const route = express.Routes();
+const route = express.Router();
 const {CreateTodo} = require("../controllers/CreateTodo")
 
 //defining the Routes
