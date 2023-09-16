7ba1abec9a5cf0bc9fa2d02b1b7ed3ad07b6b736
Aman Balyan <97960179+AmanBalyan20@users.noreply.github.com>
2023-09-16 20:39:08 +0530

    V 1.1

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -3,8 +3,21 @@
 const express = require('express');
 const route = express.Router();
 const {CreateTodo} = require("../controllers/CreateTodo")
+const {GetTodo, GetTodoByID} = require("../controllers/GetTodo")
+const {UpdateTodo} = require("../controllers/UpdateTodo")
+const {DeleteTodo} = require("../controllers/DeleteTodo")
 
 //defining the Routes
+//POST => It is used to create/Insert the data in the database
 route.post("/CreateTodo" ,CreateTodo);
 
+//GET => It is used to find or reterieve the data
+route.get("/GetTodo",GetTodo);
+route.get("/GetTodoByID/:id",GetTodoByID);
+
+//PUT => It is used to update the data 
+route.put("/UpdateTodo/:id",UpdateTodo);
+
+//DELETE =>It is used to delete the data
+route.delete("/DeleteTodo/:id",DeleteTodo)
 module.exports =route;
