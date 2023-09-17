67e02724b6b9766673949c143194648f9ea8a962
Aman Balyan <97960179+AmanBalyan20@users.noreply.github.com>
2023-09-17 15:57:06 +0530

    V 1.1

--- controllers/DeleteTodo.js
+++ controllers/DeleteTodo.js
@@ -3,7 +3,7 @@ const Todo = require("../models/TodoSchema");
 exports.DeleteTodo = async(req, res)=>{
     try{
         const {id} = req.params;
-        const todo = await Todo.findByIdAndDelete(id);
+        await Todo.findByIdAndDelete(id);
 
         res.status(200).json({
             success:true,
